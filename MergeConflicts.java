6bcc900c996d358e098a86461201d20e965b875d
Abhishek <abhishekpatil56381@gmail.com>
2025-07-21 20:06:42 +0530

    explains merge conflicts

---
+++ MergeConflicts.java
@@ -0,0 +1,11 @@
+public class MergeConflicts {
+    public static void main(String[] args) {
+        // Simulate a merge conflict scenario
+        // Version from branch 'featureA'
+        String message = "Hello from Main branch!";
+        // Version from branch 'featureB'
+        // String message = "Hello from featureB branch!";
+
+        System.out.println(message);
+    }
+}
